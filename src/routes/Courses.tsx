ad0e6d2d406e6795892d3d1d4d779001a76618e7
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-18 02:44:21 -0700

    modal

--- src/routes/Courses.tsx
+++ src/routes/Courses.tsx
@@ -1,5 +1,44 @@
-function Courses() {
-  return <></>;
-}
+import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
+const Wrapper = styled.div`
+  background-color: black;
+  margin-bottom: 150px;
+`;
+
+const Slider = styled.div`
+  position: relative;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(5, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 66px;
+`;
+
+function Courses() {
+  return (
+    <Wrapper>
+      <Slider>
+        <AnimatePresence>
+          <Row>
+            {[1, 2, 3, 4, 5].map((i) => (
+              <Box key={i}>{i}</Box>
+            ))}
+          </Row>
+        </AnimatePresence>
+      </Slider>
+    </Wrapper>
+  );
+}
 export default Courses;
